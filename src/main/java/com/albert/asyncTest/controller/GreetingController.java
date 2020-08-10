37748ffe67bc09485326cd68dbb51d12a3e906c0
albert <amyik100@gmail.com>
2020-08-10 22:09:30 +0900

    Sending Multiple Websocket Message on a request

--- src/main/java/com/albert/asyncTest/controller/GreetingController.java
+++ src/main/java/com/albert/asyncTest/controller/GreetingController.java
@@ -2,19 +2,39 @@ package com.albert.asyncTest.controller;
 
 import com.albert.asyncTest.domain.Greeting;
 import com.albert.asyncTest.domain.HelloMessage;
+import com.google.common.collect.Lists;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
+
+import java.util.List;
 
 @Controller
 public class GreetingController {
 
+    private SimpMessagingTemplate template;
+
+    @Autowired
+    public GreetingController(SimpMessagingTemplate template) {
+        this.template = template;
+    }
+
     @MessageMapping("/hello")
-    @SendTo("/topic/greetings")
-    public Greeting greeting(HelloMessage message) throws Exception {
-        Thread.sleep(1000); // simulated delay
-        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+//    @SendTo("/topic/greetings")
+    public void greeting(HelloMessage message) throws Exception {
+        List<Greeting> greetings = Lists.newArrayList();
+        greetings.add(new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!"));
+        greetings.add(new Greeting("This Message is coming"));
+        greetings.add(new Greeting("through WebSocket."));
+        greetings.add(new Greeting("It Works !"));
+
+        for(Greeting greeting : greetings) {
+            Thread.sleep(2000); // simulated delay
+            this.template.convertAndSend("/topic/greetings", greeting);
+        }
     }
 
 }
